5ced6cdcf1967658905cd5af2c77874ddfb5237f
Asım Emre Kaya <asim.emre.kaya@gmail.com>
2021-04-03 21:49:29 +0300

    form add event listener

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-})
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
